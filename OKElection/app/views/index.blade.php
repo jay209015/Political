0f57c61d393e60b6f505387d17c5f0b8ed5e09c4
unknown <APatterson@PP-01-003.epp.com>
2014-05-09 09:34:18 -0500

    Updated Database connection information

--- OKElection/app/views/index.blade.php
+++ OKElection/app/views/index.blade.php
@@ -5,5 +5,5 @@
 @stop
 
 @section('content')
- <p class="lead">Political Report is a tool to generate interesting reports from election datasets. At the present time, only the state of Oklahoma election dataset is supported. Our goal is to eventually include the entire United States - and beyond. More features coming soon.</p>
+ <p class="lead">Political Report is a tool to generate interesting reports from election datasets. At the present time, only the state of Oklahoma is supported. Our goal is to eventually include the entire United States - and beyond. More features coming soon.</p>
 @stop

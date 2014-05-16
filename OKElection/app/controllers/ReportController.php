321cd22d7c6beb719a618b3d14a0fad43ccd5c77
Jay Rivers <JRivers@printplace.com>
2014-05-15 19:06:03 -0500

    Query political affiliation
    Signed-off-by: Jay Rivers <JRivers@printplace.com>

--- OKElection/app/controllers/ReportController.php
+++ OKElection/app/controllers/ReportController.php
@@ -23,34 +23,59 @@ class ReportController extends BaseController {
         return View::make('lookupvoterinfo')->with('voter', $voter )->with('active', 'lookupvoter');
     }
 
+
+    /**
+     * Get the form to lookup voter counts
+     * @return mixed
+     */
     public function getQuery(){
         $form = [];
         $form['county_code'] = '';
         $form['election_dates'] = '';
         $form['appears'] = '';
+        $form['affiliation'] = ['options' => array(), 'selected' => ''];
+        $form['affiliation']['options']['0'] = 'All';
+        $form['affiliation']['options']['DEM'] = 'Democratic';
+        $form['affiliation']['options']['REP'] = 'Republican';
+        $form['affiliation']['options']['IND'] = 'Independent';
 
         return View::make('report/query')->with('voter', [] )->with('active','queryvoter')->with('form', $form);
     }
 
+
+    /**
+     * Process the user query and return count
+     * @return mixed
+     */
     public function postQuery(){
 
         $county = Input::get('county_code');
         $dates = explode(',', Input::get('election_dates'));
         $appears = Input::get('appears');
+        $affiliation = Input::get('affiliation');
 
         $form = [];
         $form['county_code'] = Input::get('county_code');
         $form['election_dates'] = Input::get('election_dates');
         $form['appears'] = Input::get('appears');
+        $form['affiliation'] = ['options' => array(), 'selected' => Input::get('affiliation')];
+        $form['affiliation']['options']['0'] = 'All';
+        $form['affiliation']['options']['DEM'] = 'Democratic';
+        $form['affiliation']['options']['REP'] = 'Republican';
+        $form['affiliation']['options']['IND'] = 'Independent';
 
         $query = Voter::select(array('voters.voter_id_num', DB::raw('COUNT(*) as `count`')));;
+        $query->join('histories', 'histories.voter_id_num', '=', 'voters.voter_id_num');
 
         if($county){
             $query->where('county', '=', $county);
         }
 
-        if($dates){
-            $query->join('histories', 'histories.voter_id_num', '=', 'voters.voter_id_num');
+        if($affiliation){
+            $query->where('political_affiliation', '=', $affiliation);
+        }
+
+        if(isset($dates) && is_array($dates) && $dates[0]){
             $query->wherein('election_date', $dates);
         }
 
@@ -64,6 +89,13 @@ class ReportController extends BaseController {
 
         $voters = $query->get();
 
+        /*
+        $queries = DB::getQueryLog();
+        $last_query = end($queries);
+
+        print_r($last_query);
+        exit();
+        */
 
         return View::make('report/query')->with('voters', $voters )->with('active','queryvoter')->with('form', $form);
     }
--- OKElection/app/views/report/query.blade.php
+++ OKElection/app/views/report/query.blade.php
@@ -5,30 +5,47 @@
 @stop
 
 @section('content')
-<?php if(isset($voters) && $count = $voters->count()): ?>
-<table class="table table-striped table-bordered">
-    <tr>
-        <td>Total Voters</td>
-        <td><?=$count?></td>
-    </tr>
-</table>
-<?php endif; ?>
+<?php
+$count = (isset($voters))? $voters->count(): 0;
+?>
 
-<form action="" method="POST" class="form-inline" role="form">
-    <div class="form-group">
-        <label  for="county_code">County Code</label>
-        <input type="text" class="form-control" id="county_code" name="county_code" size="2" placeholder="17" value="<?=$form['county_code']?>"/>
+<div class="row">
+    <div class="col-md-6">
+        <div class="well">
+            <form action="" method="POST" class="form" role="form">
+                <div class="form-group">
+                    <label  for="county_code">County Code</label>
+                    <input type="text" id="county_code" name="county_code" size="2" placeholder="17" value="<?=$form['county_code']?>"/>
+                </div>
+                <div class="form-group">
+                    <label  for="election_dates">Dates (comma separated)</label>
+                    <input type="text" name="election_dates" id="election_dates" placeholder="<?=date('Ymd')?>" value="<?=$form['election_dates']?>"/>
+                </div>
+                <div class="form-group">
+                    <label  for="election_dates">Political Affiliation</label>
+                    <select name="affiliation" id="affiliation">
+                        <?php foreach($form['affiliation']['options'] as $key => $value): ?>
+                        <option value="<?=$key?>" <?=(($key == $form['affiliation']['selected'])? 'SELECTED': '')?>><?=$value?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label  for="county_code">Appears in (n) of Dates</label>
+                    <input type="text" name="appears" placeholder="3" value="<?=$form['appears']?>"/>
+                </div>
+                <div class="form-group">
+                    <input type="submit" name="submit" value="Search" class="btn btn-success"/>
+                </div>
+            </form>
+        </div>
     </div>
-    <div class="form-group">
-        <label  for="election_dates">Dates (comma separated)</label>
-        <input type="text" name="election_dates" id="election_dates" placeholder="<?=date('Ymd')?>" value="<?=$form['election_dates']?>"/>
+    <div class="col-md-6">
+        <table class="table table-striped table-bordered">
+            <tr>
+                <td>Total Voters</td>
+                <td><?=$count?></td>
+            </tr>
+        </table>
     </div>
-    <div class="form-group">
-        <label  for="county_code">Appears in (n) of Dates</label>
-        <input type="text" name="appears" placeholder="3" value="<?=$form['appears']?>"/>
-    </div>
-    <div class="form-group">
-        <input type="submit" name="submit" value="Search" class="btn btn-success"/>
-    </div>
-</form>
+</div>
 @stop

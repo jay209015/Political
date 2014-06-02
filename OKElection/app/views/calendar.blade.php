c0f7cd51dcd093cb52c5fbc07f20891877f653a5
Andre Patterson <apatterson@printplace.com>
2014-06-02 09:45:12 -0500

    Added modal support for calendar.

--- OKElection/app/views/calendar.blade.php
+++ OKElection/app/views/calendar.blade.php
@@ -3,15 +3,28 @@
 @section('assets')
 <link rel='stylesheet' type='text/css' href="{{asset('css/fullcalendar.css')}}" />
 <script type='text/javascript' src="{{asset('js/fullcalendar.js')}}"></script>
+<!--Include Avgrund modal https://github.com/voronianski/jquery.avgrund.js-->
+<link rel='stylesheet' type='text/css' href="{{asset('css/avgrund.css')}}" />
+<script type='text/javascript' src="{{asset('js/jquery.avgrund.js')}}"></script>
 <script type="text/javascript">
     jQuery(document).ready(function() {
-
-        // page is now ready, initialize the calendar...
-
         jQuery('#calendar').fullCalendar({
             events: '/calendar-feed',
             dayClick: function(calEvent, jsEvent, view) {
-                window.location = '/calendar-details/' + calEvent.start + '/' + calEvent.end;
+                $('#calendar').avgrund({
+                    width: 640,
+                    height: 350,
+                    holderClass: 'custom',
+                    showClose: true,
+                    showCloseText: 'close',
+                    onBlurContainer: '.container',
+                    template: 'Candidate information goes here' +
+                        '<div>' +
+                        'Candidate Line 1' +
+                        'Candidate Line 2' +
+                        'Candidate Line 3' +
+                        '</div>'
+                });
             }
         })
 
@@ -24,5 +37,6 @@
 @stop
 
 @section('content')
-<div id="calendar"></div>
+<div id="calendar" ></div>
+
 @stop

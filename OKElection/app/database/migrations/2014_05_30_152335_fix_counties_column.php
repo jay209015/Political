eafa1fdaedd88c5752ff08e3355c1437054cb780
Jay Rivers <JRivers@printplace.com>
2014-06-02 12:28:12 -0500

    Modified county migration
    Signed-off-by: Jay Rivers <JRivers@printplace.com>

--- OKElection/app/database/migrations/2014_05_30_152335_fix_counties_column.php
+++ OKElection/app/database/migrations/2014_05_30_152335_fix_counties_column.php
@@ -12,16 +12,8 @@ class FixCountiesColumn extends Migration {
 	 */
 	public function up()
 	{
-        Schema::table('voters', function($table)
-        {
-            $table->dropColumn('county');
-            // Add the county column with index
-            $table->integer('county');
-            $table->index('county');
 
-            // Set the values for the counties to the first 2 digits of the precinct
-            DB::statement("UPDATE `voters` SET `county` = IF( LENGTH(`precinct_number`) < 6, SUBSTR(`precinct_number`,1,1), SUBSTR(`precinct_number`,1,2))");
-        });
+        DB::statement("UPDATE `voters` SET `county` = IF( LENGTH(`precinct_number`) < 6, SUBSTR(`precinct_number`,1,1), SUBSTR(`precinct_number`,1,2))");
 	}
 
 	/**

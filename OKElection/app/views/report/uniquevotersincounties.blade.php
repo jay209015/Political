c6303477e6ef93a3933c10e1e41ee546b8771c1d
Jay Rivers <JRivers@printplace.com>
2014-06-09 12:44:48 -0500

    State select for county query tool
    Signed-off-by: Jay Rivers <JRivers@printplace.com>

--- OKElection/app/views/report/uniquevotersincounties.blade.php
+++ OKElection/app/views/report/uniquevotersincounties.blade.php
@@ -33,7 +33,7 @@
                 </div>-->
                 <div class="form-group">
                     <label  for="county_code">County</label>
-                    <select name="county_code" id="county_code" ng-model="countyList" ng-options="county.name for county in counties | filter:stateList.name track by county.id">
+                    <select name="county_code" id="county_code" ng-model="countyList" ng-options="county.name for county in stateList.counties">
                     </select>
                 </div>
                 <hr>

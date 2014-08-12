5212a5dff35ea4ec154511fb7bbe5d1f561da958
Jay Rivers <JRivers@printplace.com>
2014-08-11 22:26:54 -0500

    Track and Trace
    Signed-off-by: Jay Rivers <JRivers@printplace.com>

---
+++ OKElection/app/views/report/track.blade.php
@@ -0,0 +1,16 @@
+@extends('master')
+
+@section('header')
+@stop
+
+@section('content')
+
+<div class="row col-md-12">
+    <div class="col-md-12"><h2>Track and Trace</h2></div>
+</div>
+<div class="row col-md-12">
+    <div class="col-md-12">
+        <?=$summary?>
+    </div>
+</div>
+@stop
